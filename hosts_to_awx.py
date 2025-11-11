29164651bcd5b0358bdd5b70c62bfd35175a1f50
rafaria <rafael.rafaria@hotmail.com>
2025-11-10 23:20:44 -0300

    teste invetario 2

--- hosts_to_awx.py
+++ hosts_to_awx.py
@@ -1,5 +1,6 @@
 import yaml
 import json
+import argparse
 
 # Carrega o inventário YAML
 with open('hosts.yml', 'r') as f:
@@ -24,11 +25,33 @@ def build_awx_inventory(data):
         # Adiciona hostvars
         for host, hostvars in group_data.get('hosts', {}).items():
             if isinstance(hostvars, dict):
-                result['_meta']['hostvars'][host] = hostvars
-    for group, group_data in data['all']['children'].items():
+                result['_meta']['hostvars'].setdefault(host, {}).update(hostvars)
+
+    for group, group_data in data.get('all', {}).get('children', {}).items():
         parse_group(group, group_data)
+        # Adiciona o grupo 'all' que contém todos os outros grupos como filhos
+    result['all'] = {'children': list(data.get('all', {}).get('children', {}).keys())}
     return result
 
+def get_host_vars(inventory, hostname):
+    """
+    Retorna as variáveis para um host específico.
+    """
+    return inventory.get('_meta', {}).get('hostvars', {}).get(hostname, {})
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list', action='store_true')
+    parser.add_argument('--host', type=str)
+    args = parser.parse_args()
+
     awx_inventory = build_awx_inventory(inventory)
-    print(json.dumps(awx_inventory, indent=2))
+
+    if args.list:
+        print(json.dumps(awx_inventory, indent=2))
+    elif args.host:
+        host_vars = get_host_vars(awx_inventory, args.host)
+        print(json.dumps(host_vars, indent=2))
+    else:
+        # Comportamento padrão: exibir o inventário completo
+        print(json.dumps(awx_inventory, indent=2))
